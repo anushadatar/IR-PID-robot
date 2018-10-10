7f531d913f97e161d5cc05b4867eb3eb1f769997
Tweir195 <31592190+Tweir195@users.noreply.github.com>
2018-10-10 10:43:12 -0400

    Add files via upload

--- test/generate_plot.m
+++ test/generate_plot.m
@@ -7,12 +7,14 @@ for n = 1:length(leftSensor)
 end
 %%
 t = [1:length(leftSensor)]*5;
+threshold = 950;
 figure
 clf
 subplot(2,1,1)
 plot(t,leftSensor)
 hold on
 plot(t,rightSensor)
+line([t(1) t(end)],[threshold threshold],'Color','black','LineStyle','--')
 title('Left and Right Sensor Data')
 legend('Left Sensor Data', 'Right Sensor Data','Location', 'best','Orientation','horizontal')
 ylabel('Magnitude')
